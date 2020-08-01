d25f01ef3b6674270d43e1668db4b8beaf9e894c
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-08-01 13:06:46 -0300

    adds cache creation lock

--- cache/cache.go
+++ cache/cache.go
@@ -1,29 +1,63 @@
 package cache
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	gocache "github.com/patrickmn/go-cache"
 )
 
+var (
+	ErrCacheIsBeenBuilding = fmt.Errorf("o cache já está sendo efetuado")
+)
+
 type Cache struct {
-	cache *gocache.Cache
+	cache        *gocache.Cache
+	buildingLock sync.Mutex
+	building     map[string]bool
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		cache: gocache.New(5*time.Hour, 1*time.Hour),
+		cache:    gocache.New(5*time.Hour, 1*time.Hour),
+		building: make(map[string]bool),
 	}
 }
 
 func (c *Cache) Get(key string, populateFunc func() interface{}) interface{} {
 	profilesCache, found := c.cache.Get(key)
 
+	if !found && c.isBuildingCacheFor(key) {
+		return ErrCacheIsBeenBuilding
+	}
+
 	if !found {
+		c.markAsBuilding(key)
 		data := populateFunc()
 		c.cache.SetDefault(key, data)
 		profilesCache = data
+		c.markAsDoneBuilding(key)
 	}
 
 	return profilesCache
+}
+
+func (c *Cache) isBuildingCacheFor(key string) bool {
+	c.buildingLock.Lock()
+	defer c.buildingLock.Unlock()
+	_, found := c.building[key]
+	return found
+}
+
+func (c *Cache) markAsBuilding(key string) {
+	c.buildingLock.Lock()
+	c.building[key] = true
+	c.buildingLock.Unlock()
+}
+
+func (c *Cache) markAsDoneBuilding(key string) {
+	c.buildingLock.Lock()
+	delete(c.building, key)
+	c.buildingLock.Unlock()
 }
