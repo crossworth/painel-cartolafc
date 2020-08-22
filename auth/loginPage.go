dceeac0ae460475cde84954d3d954fccb3857854
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-08-22 17:36:33 -0300

    improve login page

--- auth/loginPage.go
+++ auth/loginPage.go
@@ -5,14 +5,22 @@ const loginPage = `<!doctype html>
 <head>
   <meta charset="UTF-8">
   <title>{{ .Title }}</title>
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="/favicon.ico" type="image/x-icon">
-	<meta name="robots" content="noindex,nofollow">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <meta name="robots" content="noindex,nofollow">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
 <br>
 <center>
   <a href="/login">Fazer login</a>
 </center>
+<script>
+  var urlParams = new URLSearchParams(window.location.search)
+  var reason = urlParams.get('motivo-redirect')
+  if (reason !== '') {
+    alert(reason)
+  }
+</script>
 </body>
 </html>`
